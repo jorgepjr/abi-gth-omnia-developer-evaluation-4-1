6d406cf95bc07c814e94017592d0103c2c69afb8
Jorge Paiva <jorgepaiva1806@gmail.com>
2025-05-06 18:11:04 -0400

    fix: adjustment of foreign keys of the order class and order item

--- template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Order.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Order.cs
@@ -11,4 +11,6 @@ public class Order : BaseEntity
     
     public Guid CustomerId { get; set; }
     public Guid ShopId { get; set; }
+    public Customer Customer { get; set; } = null!;
+    public Shop Shop { get; set; } = null!;
 }
--- template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/OrderItem.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/OrderItem.cs
@@ -4,9 +4,13 @@ namespace Ambev.DeveloperEvaluation.Domain.Entities;
 
 public class OrderItem : BaseEntity
 {
-    public Guid OrderId { get; set; }
-    public Guid ProductId { get; set; }
     public int Quantity { get; set; }
     public decimal UnitPrice { get; set; }
     public bool Cancelled { get; set; } =  false;
+    
+    public Guid OrderId { get; set; }
+    public Guid ProductId { get; set; }
+    public Order Order { get; set; } = null!;
+    public Product Product { get; set; } = null!;
+    
 }
