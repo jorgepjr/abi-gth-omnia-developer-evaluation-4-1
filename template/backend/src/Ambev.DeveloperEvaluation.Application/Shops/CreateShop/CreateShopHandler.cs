44db794a961ab79cd5955c4845cf93dace730946
Jorge Paiva <jorgepaiva1806@gmail.com>
2025-05-08 00:01:48 -0400

    fix: adjusting variable names and parameter of the CreateShopHandler class

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Shops/CreateShop/CreateShopHandler.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Shops/CreateShop/CreateShopHandler.cs
@@ -1,4 +1,3 @@
-using Ambev.DeveloperEvaluation.Application.Customers.CreateCustomer;
 using Ambev.DeveloperEvaluation.Domain.Entities;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
 using AutoMapper;
@@ -8,19 +7,19 @@ namespace Ambev.DeveloperEvaluation.Application.Shops.CreateShop;
 
 public class CreateShopHandler : IRequestHandler<CreateShopCommand, CreateShopResult>
 {
-    private readonly IShopRepository _customerRepository;
+    private readonly IShopRepository _shopRepository;
     private readonly IMapper _mapper;
 
-    public CreateShopHandler(IShopRepository customerRepository, IMapper mapper)
+    public CreateShopHandler(IShopRepository shopRepository, IMapper mapper)
     {
-        _customerRepository = customerRepository;
+        _shopRepository = shopRepository;
         _mapper = mapper;
     }
 
     public async Task<CreateShopResult> Handle(CreateShopCommand command, CancellationToken cancellationToken)
     {
-        var customer = _mapper.Map<Shop>(command);
-        var createShop = await _customerRepository.CreateAsync(customer, cancellationToken);
+        var shop = _mapper.Map<Shop>(command);
+        var createShop = await _shopRepository.CreateAsync(shop, cancellationToken);
         var result = _mapper.Map<CreateShopResult>(createShop);
         return result;
     }
