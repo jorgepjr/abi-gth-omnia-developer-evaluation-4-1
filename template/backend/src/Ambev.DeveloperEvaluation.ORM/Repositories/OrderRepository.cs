e814b3b9c723761f8beb0f77570852f624929412
Jorge Paiva <jorgepaiva1806@gmail.com>
2025-05-08 21:42:36 -0400

    feat: create order sequence number

--- template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/OrderRepository.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/OrderRepository.cs
@@ -1,5 +1,6 @@
 using Ambev.DeveloperEvaluation.Domain.Entities;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ambev.DeveloperEvaluation.ORM.Repositories;
 
@@ -14,8 +15,20 @@ public class OrderRepository : IOrderRepository
 
     public async Task<Order> CreateAsync(Order order, CancellationToken cancellationToken = default)
     {
+        await SetNextOrderNumber(order, cancellationToken);
+
         await _context.Orders.AddAsync(order, cancellationToken);
         await _context.SaveChangesAsync(cancellationToken);
         return order;
+    }
+
+    private async Task SetNextOrderNumber(Order order, CancellationToken cancellationToken)
+    {
+        const long newNumber = 1;
+
+        var lastOrderRegistred = await _context.Orders
+            .OrderByDescending(x => x.Number).FirstOrDefaultAsync(cancellationToken);
+
+        order.Number = lastOrderRegistred is null ? newNumber : lastOrderRegistred.Number + 1;
     }
 }
