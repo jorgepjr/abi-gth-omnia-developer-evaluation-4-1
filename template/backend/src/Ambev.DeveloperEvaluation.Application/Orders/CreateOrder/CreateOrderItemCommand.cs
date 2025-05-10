66cbe1ffcc81493f9b6437ba5e7728bb4180ef9f
Jorge Paiva <jorgepaiva1806@gmail.com>
2025-05-09 22:37:22 -0400

    ref: remove property no use in CreateOrderItemCommand

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Orders/CreateOrder/CreateOrderItemCommand.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Orders/CreateOrder/CreateOrderItemCommand.cs
@@ -5,7 +5,6 @@ namespace Ambev.DeveloperEvaluation.Application.Orders.CreateOrder;
 
 public class CreateOrderItemCommand
 {
-    public decimal UnitPrice { get; set; }
     public Guid ProductId { get; set; }
     public int Quantity { get; set; }
     
