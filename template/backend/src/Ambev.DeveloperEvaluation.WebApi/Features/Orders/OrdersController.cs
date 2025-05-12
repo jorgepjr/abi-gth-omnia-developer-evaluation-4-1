b4f4164873091ba56698207a4a1cde54ec70f1cb
Jorge Paiva <jorgepaiva1806@gmail.com>
2025-05-12 10:11:59 -0400

    chore: add documentation

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Orders/OrdersController.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Orders/OrdersController.cs
@@ -28,6 +28,9 @@ public class OrdersController : BaseController
         _mapper = mapper;
     }
 
+    /// <summary>Create order and add order items</summary>
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [EndpointSummary("Create order and add order items")]
     [HttpPost]
     public async Task<IActionResult> CreateOrder([FromBody] CreateOrderRequest request, CancellationToken cancellationToken)
     {
@@ -54,6 +57,9 @@ public class OrdersController : BaseController
         }); 
     }
     
+    /// <summary>Update order</summary>
+    [ProducesResponseType(typeof(UpdateOrderResult), StatusCodes.Status200OK)]
+    [EndpointSummary("Update order")]
     [HttpPut("{orderId}")]
     public async Task<IActionResult> UpdateOrder(Guid orderId, [FromBody] UpdateOrderRequest request, CancellationToken cancellationToken)
     {
@@ -64,6 +70,9 @@ public class OrdersController : BaseController
        return Ok(response); 
     }
     
+    /// <summary>Cancel order  by order id </summary>
+    [ProducesResponseType(typeof(List<CancelOrderItemsReult>), StatusCodes.Status200OK)]
+    [EndpointSummary("Cancel order by orderId")]
     [HttpPut("{orderId}/cancel")]
     public async Task<IActionResult> Cancel(Guid orderId, CancellationToken cancellationToken)
     {
@@ -71,6 +80,9 @@ public class OrdersController : BaseController
         return Ok(response); 
     }
     
+    /// <summary>Cancel order items by order id and orderItems</summary>
+    [ProducesResponseType(typeof(List<CancelOrderItemsReult>), StatusCodes.Status200OK)]
+    [EndpointSummary("Cancel order items by orderId and orderItems")]
     [HttpPut("{orderId}/cancel-items")]
     public async Task<IActionResult> CancelOrderItems(Guid orderId, List<Guid?>? orderItemsIds, CancellationToken cancellationToken)
     {
@@ -78,7 +90,14 @@ public class OrdersController : BaseController
             new CancelOrderItemsCommand{OrderId = orderId, OrderItemsIds = orderItemsIds}, cancellationToken);
         return Ok(response); 
     }
-    
+
+    /// <summary>
+    /// Get order by orderId
+    /// </summary>
+    /// <param name="orderId">orderId</param>
+    /// <param name="cancellationToken"></param>
+    [ProducesResponseType(typeof(GetOrderByReult), StatusCodes.Status200OK)]
+    [EndpointSummary("Get order by id")]
     [HttpGet("{orderId}")]
     public async Task<IActionResult> GetById(Guid orderId, CancellationToken cancellationToken)
     {
@@ -86,6 +105,10 @@ public class OrdersController : BaseController
         return Ok(response);
     }
     
+    /// <summary>Remove order by orderId</summary>
+    /// <param name="orderId">orderId</param>
+    [ProducesResponseType(typeof(DeleteOrderReult), StatusCodes.Status200OK)]
+    [EndpointSummary("Delete order by id")]
     [HttpDelete("{orderId}")]
     public async Task<IActionResult> Delete(Guid orderId, CancellationToken cancellationToken)
     {
@@ -93,7 +116,15 @@ public class OrdersController : BaseController
         return Ok(response);
     }
     
+    /// <summary>
+    /// Returns a paginated list of orders.
+    /// </summary>
+    /// <param name="pageNumber">Page number</param>
+    /// <param name="pageSize">Number of items per page</param>
+    /// <param name="cancellationToken"></param>
     [HttpGet("{pageNumber}/{pageSize}")]
+    [ProducesResponseType(typeof(GetAllOrdersReult), StatusCodes.Status200OK)]
+    [EndpointSummary("Get all orders with pagination")]
     public async Task<IActionResult> GetAllOrders(int pageSize, int pageNumber, CancellationToken cancellationToken)
     {
         var response = await _mediator.Send(
